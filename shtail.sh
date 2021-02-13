27c19926eb6dd4c3a197db9492892c034633d653
makarai <17119942+ma-karai@users.noreply.github.com>
2021-02-13 14:00:31 +0100

    Update shtail.sh

--- shtail.sh
+++ shtail.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # " /bin/sh /Shared/nginx/shtail.sh & " needs to be added to /startapp.sh as the second line
-tail -f /config/log/nginx/proxy_host* | grep -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | while read line;
+tail -f /logs/proxy_host* | grep -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | while read line;
 do
   domain=`echo ${line:0:80} | grep -m 1 -o -e "[a-z0-9]*\.[a-z0-9]*\.ddns.net" -e "[a-z0-9]*\.ddns.net"`
   ipaddressnumber=`echo $line | grep -o -m 1 -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep -v "192.168.0.*"`  ##UPDATE grep -v with local network
@@ -14,7 +14,7 @@ do
   if "$ipaddressnumber" == "$HomeIP";then
     echo "Home IP"
   else
-    /usr/bin/python3 /Shared/nginx/Getipinfo.py "$ipaddressnumber" "$domain" "$length"
+    /usr/bin/python3 $N-P-M-GRAF_HOME/Getipinfo.py "$ipaddressnumber" "$domain" "$length"
   fi
 done
 reboot
