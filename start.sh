bde4fb57254c065e4c20ae5bc732197e234fce3c
makarai <17119942+ma-karai@users.noreply.github.com>
2021-02-13 14:58:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "lets go and send connection info to influx"
 /usr/bin/nohup python -u $N-P-M-GRAF_HOME/Getipinfo.py
+
+sleep 0.5
+tee $N-P-M-GRAF_HOME/nohup.out > /proc/1/fd/1 2>/proc/1/fd/2
