d7e24bef125ada3d0ca5e61ec5a7e1006f23ebf3
makarai <17119942+ma-karai@users.noreply.github.com>
2021-02-13 15:21:12 +0100

    Update shtail.sh

--- shtail.sh
+++ shtail.sh
@@ -14,7 +14,7 @@ do
   if "$ipaddressnumber" == "$HomeIP";then
     echo "Home IP"
   else
-    /usr/bin/python3 $N-P-M-GRAF_HOME/Getipinfo.py "$ipaddressnumber" "$domain" "$length"
+    /usr/bin/python3 $NPMGRAF_HOME/Getipinfo.py "$ipaddressnumber" "$domain" "$length"
   fi
 done
 reboot
